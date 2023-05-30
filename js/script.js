6b449d08c9925a3c03cea902d2e692fdbca1bc9d
wiraafriyanip <wiraafriyanip@gmail.com>
2023-05-30 15:53:30 +0700

    mario stack ketika menyentuh pipa

--- js/script.js
+++ js/script.js
@@ -11,7 +11,9 @@ const jump = () => {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft
-    if (pipePosition <= 120) {
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','')
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none"
         pipe.style.left = `${pipePosition}px`
     }
