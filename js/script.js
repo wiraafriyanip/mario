423e7ded1ac099778b737ab5fae8185c8130ce3b
wiraafriyanip <wiraafriyanip@gmail.com>
2023-05-31 15:16:22 +0700

    mario nabrak game over

--- js/script.js
+++ js/script.js
@@ -16,6 +16,11 @@ const loop = setInterval(() => {
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = "none"
         pipe.style.left = `${pipePosition}px`
+        mario.style.animation ="none"
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = './images/game-over.png'
+        mario.style.width = "75px"
+        mario.style.marginLeft = "50px"
     }
 }, 10);
 document.addEventListener('keydown', jump)
